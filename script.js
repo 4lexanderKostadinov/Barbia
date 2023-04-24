0bdca60494d56fef9f8e8eae4c20a702eefe3883
Aleksandar Kostadinov <alexander.kostadinov06@gmail.com>
2023-04-24 12:30:15 +0300

    Catalog Finished

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 toggleButton.addEventListener("click", () => {
     navbarLinks.classList.toggle("active");
 });
+
+const preveiwContainer = document.querySelector(".products-preview");
+const previewBox = preveiwContainer.querySelectorAll(".preview");
+
+document.querySelectorAll(".products-container .product").forEach((product) => {
+    product.onclick = () => {
+        preveiwContainer.style.display = "flex";
+        let name = product.getAttribute("data-name");
+        previewBox.forEach((preview) => {
+            let target = preview.getAttribute("data-target");
+            if (name == target) {
+                preview.classList.add("active");
+            }
+        });
+    };
+});
+
+previewBox.forEach((close) => {
+    close.querySelector(".fa-times").onclick = () => {
+        close.classList.remove("active");
+        preveiwContainer.style.display = "none";
+    };
+});
